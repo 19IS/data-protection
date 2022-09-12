49bc50bbf3672a15334ee9ab0e693e19af756d78
Hunter-99 <andreytitan2001@gmail.com>
2022-09-12 21:25:08 +0300

    Added 2-nd lab

---
+++ labs/lab-2/lab-2.py
@@ -0,0 +1,64 @@
+from rich import print
+from rich.prompt import Prompt
+
+# Функция для кодирования исходного сообщения
+def encode_message(message: list, shift: int, alphabet: list) -> str:
+    message = list(message)
+    for index, char in enumerate(message):
+        if char in alphabet:
+            message[index] = alphabet[(alphabet.index(char) + shift) % len(alphabet)]
+    return ''.join(message)
+
+# Функция для расшифровки исходного сообщеня
+def decode_message(message: list, shift: int, alphabet: list) -> str:
+    message = list(message)
+    for index, char in enumerate(message):
+        if char in alphabet:
+            message[index] = alphabet[(alphabet.index(char) - shift) % len(alphabet)] 
+    return ''.join(message)
+
+
+def run():
+    russian_alphabet = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+    message = Prompt.ask("[bold blue]Введите сообщение[/]")
+    mode = Prompt.ask("[bold blue]Выберите действие для сообщения[/]", choices=["зашифровать", "расшифровать"])
+    
+    if mode == "зашифровать":
+        shift = int(Prompt.ask("[bold blue]Введите значение сдвига[/]"))
+        
+        if shift == 0:
+            print(f"[bold yellow]Значение сдвига равно 0. Закодированное сообщение будет идентично исходному![/]")
+        elif shift > len(russian_alphabet):
+            print(f"[bold yellow]Значение сдвига больше {len(russian_alphabet)}. Значение сдвига автоматически будет равно: {shift % len(russian_alphabet)}![/]")
+            
+        encoded_message = encode_message(
+            message=message,
+            shift=shift,
+            alphabet=russian_alphabet
+        )
+        print(f"Зашифрованное сообщение: {encoded_message}")
+        decoded_message = decode_message(
+            message=encoded_message,
+            shift=shift,
+            alphabet=russian_alphabet
+        )
+        print(f"Дешифрованное сообщение: {decoded_message}")
+        if decoded_message == message:
+            print(f"[bold green]Алгоритм шифрования отработал корректно![/]")
+        else:
+            print(f"[bold red]Алгоритм шифрования отработал некорректно! Исходное сообщение не совпадает с расшифрованным.[/]")
+            
+    elif mode == "расшифровать":
+        # щъзшмсарс р цлрх рп цщхцйхгэ щчцщцицй пзбръг рхьцшфзюрр еъц щруцйгм фмъцлг, ъц мщъд цэшзхз лцтыфмхъз (9-я итерация будет верной)
+        for iteration in range(len(russian_alphabet)):
+            user_input = Prompt.ask(f"[bold blue]Данное сообщение ({decode_message(message=message, shift=iteration, alphabet=russian_alphabet)}) является корректым?[/]", choices=['да', 'нет'])
+            if user_input.lower() in ['да', 'д']:
+                print(f"[bold green]Завешраю работу![/]")
+                break
+            elif user_input.lower() in ['нет', 'н']:
+                continue
+                
+                
+if __name__ == "__main__":
+    run()
+
